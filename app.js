5eba07f2c488cdcdffdbd7e666a72496d1262b21
Kirill <freshjs.ftd@gmail.com>
2021-11-17 11:28:23 +0200

    Add cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandlers')
 const app = express();
+
+app.use(cors());  // разрешение на подключение к серверу, связано с политикой безопасности браузеров и серверов
 
 app.use(express.json());
 
