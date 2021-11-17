c7044f1aef2cb30a6f8ef13f68115d86a4da6820
Kirill <freshjs.ftd@gmail.com>
2021-11-17 11:28:15 +0200

    Add static file handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.static('public')); // отдача статических файлов из папки public
+
 app.use('/api', router);
 
 app.use(errorHandler);
