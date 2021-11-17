2a3adcac77d254cb69150fb3e9e9ca436a88a460
Kirill <freshjs.ftd@gmail.com>
2021-11-17 11:26:06 +0200

    Add error handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const errorHandler = require('./middlewares/errorHandlers')
 const app = express();
 
 app.use(express.json());
+
+app.use(errorHandler);
 
 module.exports = app;
